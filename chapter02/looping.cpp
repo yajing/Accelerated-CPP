461598e560b612d7ef7393ed642269e3af39bd40
ycrong <ycrong@yajing.(none)>
2013-05-19 10:04:36 +0800

    chapater 2 looping and counting

---
+++ chapter02/looping.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+//ask for the person's name
+cout << "Please enter your name: ";
+
+//read the name
+string name;
+cin >> name;
+
+const string greeting = "hello, " + name + " !";
+
+const int pad = 1;
+
+const int rows = pad * 2 + 3;
+const string::size_type cols = greeting.size() + pad * 2 + 2;
+
+cout << endl;
+
+for (int r = 0; r != rows; r++)
+{
+    string::size_type c = 0;
+    
+    while (c != cols)
+    {
+        if ( r == pad + 1 && c == pad + 1)
+	{
+	   cout << greeting;
+	   c += greeting.size();
+	}
+	else
+	{
+	   if ( r == 0 || r == rows - 1 ||
+		c == 0 || c == cols - 1)
+		cout << "*";
+	   else
+		cout << " ";
+	   c++;
+	}
+    }
+    
+    cout << endl;
+}
+return 0;
+}
